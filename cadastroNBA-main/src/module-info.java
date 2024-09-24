bf69457344181973a9acc4c0e8c2e65690048e3a
lab04 <lab04@lab4-17>
2024-09-23 22:34:31 -0300

    third commit

--- cadastroNBA-main/src/module-info.java
+++ cadastroNBA-main/src/module-info.java
@@ -2,6 +2,7 @@ module LAAnalytics {
 	requires javafx.controls;
 	requires javafx.fxml;
 	requires javafx.graphics;
+	requires java.sql;
 	
 	opens usa.com.nba.atletas to javafx.graphics, javafx.fxml;
 }
--- cadastroNBA-main/src/usa/com/nba/atletas/operacoesController.java
+++ cadastroNBA-main/src/usa/com/nba/atletas/operacoesController.java
@@ -1,5 +1,19 @@
 package usa.com.nba.atletas;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class operacoesController {
+	@FXML
+	private TextField txt_nome;
+	@FXML
+	private TextField txt_sobrenome;
+	@FXML
+	private TextField txt_idade;
+	@FXML
+	private TextField txt_posicao;
+	@FXML
+	private TextField txt_equipe;
+	
 	
 }
